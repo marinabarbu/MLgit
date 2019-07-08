8b10b0f738ae4e1ef61485d6c790389d377b1d03
Tehnic <barbumarina.madalina97@gmail.com>
2019-07-08 15:35:24 +0300

    made a script which makes one graph for each day

--- multiple_graph_and_days.py
+++ multiple_graph_and_days.py
@@ -12,7 +12,7 @@ PM10_data = []
 
 #sorting the data and put every date in the specific list
 for i in range(len(type_list)):
-    if type_list[i] == "PM10":
+    if type_list[i] == "PRES":
         PM10_time.append(time_list[i])
         PM10_data.append(data_list[i])
 
@@ -23,78 +23,70 @@ for i in range(len(PM10_time)):
 #print(len(PM10))
 PM10 = []
 
-days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18',
-        '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+PM10_time.insert(0,'24-10-18')
 
-months= ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+unic_days = []
+unic_days.append(PM10_time[0][0:8])
 
-years=['18', '19']
-
-print("days: " + str(days))
-print("months: " + str(months))
-
-#print(start, stop)
-
-for i in range(len(PM10_time)):
-    try:
-
-        x_data, y_data, x_data_days_hours = [], [], []
-        # print(PM10[i])
-        start = 0
-        stop = 0
-
-        for y in years:
-            for m in months:
-                for d in days:
-                    try:
-                        if PM10_time[i][0:8].strip() == d + '-' + m + '-' + y:
-                            print(d + '-' + m + '-' + y)
-                            start = i
-                        elif PM10_time[i][0:8].strip() == days[days.index(d) + 1]+ '-' + m + '-' + y:
-                            stop = i
-                    except:
-                        pass
-        for i in range(start+1, stop+1):
-            #print(PM10[i])
-            x_data.append(i)
-            y_data.append(PM10_data[i])
-            x_data_days_hours.append(PM10_time[i] + " " + PM10_time[i])
-
-        print(len(x_data))
-        print(len(y_data))
-        x = []
-        for i in range(len(x_data)):
-            x.append(i)
-
-        print(x)
-        print(y_data)
-        x_data = np.array([x_data]).T
-        x = np.array([x]).T
-        y_data = np.array(y_data)
-
-        '''
-        print("x: " + str(x))
-        print("x data: " + str(x_data))
-        print("y data: " + str(y_data))
-        print(x.shape)
-        print(x_data.shape)
-        print(y_data.shape)
-        
-        '''
-        svr_rbf = SVR(kernel='rbf', C=1e5, gamma=0.3)
-        y_rbf = svr_rbf.fit(x, y_data).predict(x)
-        lw = 2
-        plt.show()
-        #plt.scatter(x, y_data, color='black', label='data')
-        plt.plot(x, y_rbf, color='red', lw=3, label='RBF model')
-        plt.ylim(bottom=0)
-        plt.ylim(top=200)
-        plt.xlabel('data')
-        plt.ylabel('target')
-        plt.title('Support Vector Regression')
-        plt.legend()
-        plt.show()
-    except:
+for d in PM10_time:
+    if d[0:8] in unic_days:
         pass
+    else:
+        unic_days.append(d[0:8])
 
+for d in unic_days:
+    start = 0
+    stop = 0
+    for i in range(len(PM10_time)):
+        if PM10_time[i][0:8] == d:
+            start = i
+        elif PM10_time[i][0:8] == unic_days[unic_days.index(d)+1]:
+            stop = i
+
+    x_data, y_data, x_data_days_hours = [], [], []
+
+    for i in range(start + 1, stop + 1):
+        # print(PM10[i])
+        x_data.append(i)
+        y_data.append(PM10_data[i])
+        x_data_days_hours.append(PM10_time[i] + " " + PM10_time[i])
+
+    print("start is: " + str(start))
+    print("stop is: " + str(stop))
+    print(len(x_data))
+    print(len(y_data))
+    x = []
+
+    for i in range(len(x_data)):
+        x.append(i)
+
+    y_data=np.array(y_data).ravel()
+    print(x)
+    print(y_data)
+    x_data = np.array([x_data]).T
+    x = np.array([x]).T
+    y_data = np.array(y_data)
+
+    '''
+    print("x: " + str(x))
+    print("x data: " + str(x_data))
+    print("y data: " + str(y_data))
+    print(x.shape)
+    print(x_data.shape)
+    print(y_data.shape)
+    '''
+
+    svr_rbf = SVR(kernel='rbf', C=1e5, gamma=0.3)
+    y_rbf = svr_rbf.fit(x, y_data).predict(x)
+    plt.show()
+    #plt.scatter(x, y_data, color='black', label='data')
+    plt.plot(x, y_rbf, color='red', lw=3, label='RBF model')
+    #plt.ylim(bottom=0)
+    #plt.ylim(top=200)
+    plt.xlabel('data')
+    plt.ylabel('grad de poluare')
+    plt.title(days_of_the_week[(unic_days.index(d)+3)%7] + " " + unic_days[unic_days.index(d)+1])
+    plt.legend()
+    plt.show()
 
