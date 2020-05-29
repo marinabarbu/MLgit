b13e7691013813321494069c8247a44fa7e3fae2
Marina Barbu <barbumarina.madalina97@gmail.com>
2020-05-29 13:27:15 +0300

    mlp on pm10 script

--- mlp_exemple.py
+++ mlp_exemple.py
@@ -1,6 +1,8 @@
 from sklearn.neural_network import MLPClassifier
 
 X = [[0., 0.], [1., 1.]]
+# input = 3 neuron : ora_crt, cantit_pm_ora_Crt, ora_urmat
+#output: 1 sg neuron: cantit_pm_ora_urmat
 y = [0, 1]
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(X, y)
--- mlp_pm10.py
+++ mlp_pm10.py
@@ -1,10 +1,11 @@
 import numpy as np
-from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 import math
+from sklearn.neural_network import MLPClassifier
+import torch
 
 dates, values = [], []
 
@@ -25,8 +26,7 @@ PM10 = []
 
 while i < len(PM10_time):
     #print(HUM_time[i][9:14])  #selecting the hour
-    hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-         12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
     for h in hours:
         try:
             while PM10_time[i][9:11] == h:
@@ -37,7 +37,6 @@ while i < len(PM10_time):
             nr_masuratori = len(l)
             element = []
             element.append(PM10_time[i][:9])
-            # print(h)
             element.append(h)
             element.append(nr_masuratori)
             element.append(medie)
@@ -46,30 +45,19 @@ while i < len(PM10_time):
         except:
             pass
 
+for data in PM10:
+    print(data)
+
 X, y = [], []
 
-for i in range(1, len(PM10)):
+for i in range(1,len(PM10)):
     X.append([PM10[i-1][1], PM10[i-1][3], PM10[i][1]])
     y.append(PM10[i][3])
 
-# print(X)
-# print(y)
-
-print("X length: ", len(X))
-print("y length: ", len(y))
+print(X)
+print(y)
 
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 clf.fit(X, y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
-
-print("x train length: ", len(X_train))
-print("y train length: ", len(y_train))
-
-print("x test length: ", len(X_test))
-print("y test length: ", len(y_test))
-
-# clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-# clf.fit(X_train, y_train)
-
-# print(clf.predict([[2., 2.], [-1., -2.]]))
+torch.save(clf.state_dict(), "model_clf.pt")
