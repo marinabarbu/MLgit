fdee94fe1ff141e9ce3133211cc752c4e2cccd05
Marina Barbu <barbumarina.madalina97@gmail.com>
2020-06-29 17:33:29 +0300

    subscribe_grafana.py script to get online data

--- subscribe_grafana.py
+++ subscribe_grafana.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import re
+import numpy as np
 
 #topic = 'meshliumfa30/WINSHI/#'
 #topic = 'odsi/raspberry/marina_barbu'
@@ -22,28 +23,49 @@ def on_message(client, userdata, msg):
     print(m)
     text = re.findall('"([^"]*)"', m) #selectarea string-urilor dintre ghilimele
     # print(text)
-    value = text[9]
+    value = float(text[9])
     timestamp = text[11]
     hour = timestamp[11:13]
+    print(str(hour))
+    print(type(value))
     print("value: ", value)
     print("Timestamp: ", timestamp)
-    print("Hour: ", hour)
+    print("Hour: ", float(hour))
 
     if len(lista_ore) > 0:
         if lista_ore[-1][0] == hour:
             lista_ore.append([hour, float(round(value))])
             print(lista_ore)
+            print(float(round(float(hour))), float(round(value)), float(round(float(hour))) + 1)
+            pred = clf.predict([float(round(float(hour))), float(round(value)), float(round(float(hour))) + 1])
+            print("Predicted: ",pred)
         else:
             mean = sum([x[1] for x in lista_ore])/len(lista_ore)
             print([float(lista_ore[-1][0]), float(round(mean)), float(hour)])
-            predict = clf.predict([float(lista_ore[-1][0]), float(round(mean)), float(hour)])
-            print(predict)
+            print([float(round(float(lista_ore[-1][0]))), float(round(mean)), float(round(float(hour)))])
+            # predict = clf.predict([float(lista_ore[-1][0]), float(round(mean)), float(hour)])
+            predict = clf.predict([float(round(float(lista_ore[-1][0]))), float(round(mean)), float(round(float(hour)))])
+            print("Predict: ", predict)
             lista_ore.clear()
             lista_ore.append([hour, float(round(value))])
             print(lista_ore)
     else:
-        lista_ore.append([hour, float(round(value))])
+        lista_ore.append([hour, float(round(float(value)))])
         print(lista_ore)
+        l = []
+        l.append(float(round(float(hour))))
+        l.append(float(round(value)))
+        l.append(float(round(float(hour))) + 1)
+        # l = [float(round(float(hour))), float(round(value)), float(round(float(hour))) + 1]
+        print(type(l[0]), type(l[1]), type(l[2]))
+        l[1] = np.float64(l[1])
+        print(type(l[1]))
+        #values = np.frombuffer(values, dtype=np.float64)
+        print(type(l[0]), type(l[1]), type(l[2]))
+        pred = clf.predict(l)
+        print("Predicted: ", pred)
+        pred = clf.predict([l])
+        print("Predicted: ", pred)
 
 
 client = mqtt.Client()
